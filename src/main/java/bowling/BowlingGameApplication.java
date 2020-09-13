9efbc45d70632064d68eab7e3585818e0805151b
John <bass0223@naver.com>
2020-09-13 18:25:22 +0900

    도메인 로직과 뷰 로직의 변화에 따른 컨트롤러 로직 수정

--- src/main/java/bowling/BowlingGameApplication.java
+++ src/main/java/bowling/BowlingGameApplication.java
@@ -1,22 +1,25 @@
 package bowling;
 
+import java.util.List;
+
 import bowling.frame.Frames;
 import bowling.pin.Pins;
-import bowling.user.Player;
+import bowling.user.Players;
 import bowling.view.InputView;
 import bowling.view.OutputView;
 
 public class BowlingGameApplication {
 
 	public static void main(String[] args) {
-		Player player = Player.of(InputView.inputPlayerName());
-		BowlingGame bowlingGame = BowlingGame.init(player);
-		OutputView.viewInit(player);
+		int countOfPlayer = InputView.inputCountOfPlayer();
+		Players players = Players.of(InputView.inputPlayerNames(countOfPlayer));
+		BowlingGame bowlingGame = BowlingGame.init(players);
+		OutputView.viewInit(players);
 
 		while (!bowlingGame.isEndGame()) {
-			int knockingDownPins = InputView.inputPitchingOf(bowlingGame.getCurrentFrameNo());
-			Frames frames = bowlingGame.pitchBall(Pins.of(knockingDownPins));
-			OutputView.viewPitchingResult(bowlingGame.getPlayer(), frames);
+			int knockingDownPins = InputView.inputPitchingOf(bowlingGame.getCurrentPlayer());
+			List<Frames> framesList = bowlingGame.pitchBall(Pins.of(knockingDownPins));
+			OutputView.viewPitchingResults(players, framesList);
 		}
 
 	}
